22c8c2cb56d0b4e7886436bce3a01ddfc1a7d466
ankit-birgade27 <ankitbirgade@gmail.com>
2025-02-23 20:49:33 +0530

    showing todays date

--- src/com/tka/Demo.java
+++ src/com/tka/Demo.java
@@ -1,8 +1,12 @@
 package com.tka;
 
+import java.util.Date;
+
 public class Demo {
 
     public static void main(String[] args) {
         System.out.println("i m learning java");
+        System.out.println(new Date());
+
     }
 }
